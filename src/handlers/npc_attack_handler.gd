3553856e50d4fdd7ba4dd9f20c2cb5badd9f7d2b
Michael Peters <michael24peters@gmail.com>
2024-07-23 06:53:37 +0200

    fix: state machine now enters move state based on ray detection; rays dont update properly

--- src/handlers/npc_attack_handler.gd
+++ src/handlers/npc_attack_handler.gd
@@ -3,7 +3,6 @@ extends InputHandler
 @export var agent: Agent
 
 func want_attack():
-	if agent.current_action["name"] == "attack":
-		return true
+	if agent.current_action["name"] == "attack": return true
 	return false
 
